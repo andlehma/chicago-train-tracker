25897f53b120fbf54b2359c83211f9e4f972f104
andlehma <andrew.lehman364@gmail.com>
2020-06-14 16:20:46 -0500

    skeleton EtaCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from "react";
 import StopSelector from "./StopSelector";
+import EtaCard from "./EtaCard";
 
 function App() {
     const [stop, setStop] = useState({
         id: 30050, name: "State/Lake (Inner Loop)"
     });
+    const [etas, setEtas] = useState([]);
+
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         let endpoint = `/api/${stop.id}`;
         fetch(endpoint)
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => {
+                if (data.ctatt.eta) {
+                    setEtas(data.ctatt.eta);
+                } else {
+                    setEtas([]);
+                }
+                setLoading(false);
+            });
     }, [stop])
 
     return (
         <>
             <StopSelector currentStop={stop} callback={setStop} />
+            {loading ?
+                <p>loading...</p> :
+                <div id="etas-container">
+                    {etas.map((eta, i) =>
+                        <EtaCard
+                            key={i}
+                            arrivalTime={eta.arrT}
+                            route={eta.rt}
+                            destinationName={eta.destNm}
+                        />
+                    )}
+                </div>
+            }
         </>
     );
 }
